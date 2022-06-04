ef302f36b145133356f39afbb8ded8d2c713ca77
Chris Hayes <6013871+Christopher-Hayes@users.noreply.github.com>
2022-06-04 08:48:01 -0400

    fix: local electron-build config import

--- src/scripts/build-electron.ts
+++ src/scripts/build-electron.ts
@@ -1,6 +1,8 @@
 import { build } from 'electron-builder'
-import config from '../config/electron-builder'
+import { build as config } from '../../package.json'
 
 export default function () {
-  return build({ config })
+  // "as any" used to suppress TS error over MacOS "arch" object type
+  // "as any" используется для предотвращения ошибки TS при типе объекта "arch" на MacOS
+  return build({ config } as any)
 }
